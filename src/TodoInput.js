73a7b0a9957a038234d26e6183af20ec8a78f5f6
Jana2xN <jana.neirinck@hotmail.com>
2024-02-26 15:09:37 +0100

    Add const TodoInput, const handleInputChange and const handleAddTodo in TodoInput.js src

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,1 +1,25 @@
 import React, { useState } from 'react';
+
+const TodoInput = ({ addTodo }) => {
+    const [todoText, setTodoText] = useState('');
+
+    const handleInputChange = (e) => {
+        setTodoText(e.target.value);
+    };
+
+    const handleAddTodo = () => {
+        if (todoText.trim() !== '') {
+        addTodo(todoText);
+        setTodoText('');
+        }
+    };
+
+    return (
+        <div>
+            <input type="text" value={todoText} onChange={handleInputChange} />
+            <button onClick={handleAddTodo}>Add Todo</button>
+        </div>
+    );
+};
+
+export default TodoInput;
