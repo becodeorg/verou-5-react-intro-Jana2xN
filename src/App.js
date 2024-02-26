003f3dcb13d6d315e2729766f1f441d99b9f765c
Jana2xN <jana.neirinck@hotmail.com>
2024-02-26 14:42:54 +0100

    Import TodoInput in App.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import './styles.css'
+import React, { useState } from 'react';
+import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
 const App = () => {
-    return (
-        <div>
-            <TodoList />
-        </div>
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Learn React', completed: false },
+    { id: 2, text: 'Build a Todo App', completed: true },
+    { id: 3, text: 'Deploy the App', completed: false },
+  ]);
+
+  const addTodo = (text) => {
+    const newTodo = { id: todos.length + 1, text, completed: false };
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
     );
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <TodoInput addTodo={addTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+    </div>
+  );
 };
 
 export default App;
